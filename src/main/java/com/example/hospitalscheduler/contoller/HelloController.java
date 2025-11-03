f8a5a18e1c1fbf1c46ad837fc36061a6631e2970
Estella1 <estella.santos.moreira@gmail.com>
2025-11-02 22:24:45 -0300

    first commit

--- src/main/java/com/example/hospitalscheduler/contoller/HelloController.java
+++ src/main/java/com/example/hospitalscheduler/contoller/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.hospitalscheduler.controller;
+package com.example.hospitalscheduler.contoller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
